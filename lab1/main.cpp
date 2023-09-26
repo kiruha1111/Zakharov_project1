6179290b89ca224cf7231fc9b3b4171d8bb28ad5
kiruha1111 <zakharovkirill.school@gmail.com>
2023-09-26 13:26:02 +0300

    Add menu

---
+++ lab1/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	cout << "1. Add a pipe" << endl;
+	cout << "2. Add a compressor station" << endl; 
+	cout << "3. Watch all objects" << endl; 
+	cout << "4. Edit a pipe" << endl; 
+	cout << "5. Edit a compressor station" << endl; 
+	cout << "6. Save" << endl; 
+	cout << "7. Download from file" << endl; 
+	cout << "8. Exit" << endl;
+	return 0;
+}
